7c0fab77da0c2753b6d6fce418e8d52a9d973ccf
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-05 20:15:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT s.title, COUNT(c.species) FROM series s INNER JOIN books b ON s.id = b.series_id INNER JOIN "
+  "character_books cb ON b.id = cd.book_id INNER JOIN characters c ON cb.character_id = c.id "
+  "WHERE c.species = 'human' GROUP BY s.title ORDER BY s.title DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
